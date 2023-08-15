925e99bc549a8629287740e2745cd66195c11669
Evgeniy Selivanov <genyaselivanovzp@gmail.com>
2023-08-15 12:29:19 +0300

    add navigation with  handleBackButton

--- screens/WebViewScreen.jsx
+++ screens/WebViewScreen.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { WebView } from 'react-native-webview';
+import { BackHandler } from 'react-native';
+
+
+
 const WebViewScreen = () => {
+  const webViewRef = useRef(null);
+  
+  const handleBackButton = () => {
+    if (webViewRef.current) {
+        webViewRef.current.goBack();
+        return true; // Вернуть true, чтобы предотвратить выход из приложения
+    }
+    return false; // Вернуть false, чтобы дать приложению обработать нажатие
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', handleBackButton);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', handleBackButton);
+    };
+  }, []);
+
   return (
     <WebView
+      ref={webViewRef}
       source={{ uri: 'https://zorbleizzle.com' }} // Замените на нужный URL
-      style={{ flex: 1 }} 
+      style={{ flex: 1 }}
       javaScriptEnabled={true} // Разрешить выполнение JavaScript
       sharedCookiesEnabled={true} // (только для iOS) Разрешить совместное использование куков с Safari
     />
   );
-}
+};
 export default WebViewScreen;
- 
